988a9cababb2f30329d20ff27d3aaaf2ec2d60ed
Buratud Thumboon <buratud@gmail.com>
2025-04-01 17:48:44 +0700

    Change frequency

--- src/app/(main)/imu/page.tsx
+++ src/app/(main)/imu/page.tsx
@@ -70,13 +70,13 @@ export default function IMU() {
           <AccelerationChart data={sensorData} />
         </div>
         <div style={{ gridArea: "frequency_x" }}>
-          <FrequencyChart data={accelX} frequency={500} size={1024} title="X-Axis FFT" color="#ff0000" />
+          <FrequencyChart data={accelX} frequency={200} size={1024} title="X-Axis FFT" color="#ff0000" />
         </div>
         <div style={{ gridArea: "frequency_y" }}>
-          <FrequencyChart data={accelY} frequency={500} size={1024} title="Y-Axis FFT" color="#00ff00" />
+          <FrequencyChart data={accelY} frequency={200} size={1024} title="Y-Axis FFT" color="#00ff00" />
         </div>
         <div style={{ gridArea: "frequency_z" }}>
-          <FrequencyChart data={accelZ} frequency={500} size={1024} title="Z-Axis FFT" color="#0000ff" />
+          <FrequencyChart data={accelZ} frequency={200} size={1024} title="Z-Axis FFT" color="#0000ff" />
         </div>
       </div>
       {isRunning && shaking && (<>
